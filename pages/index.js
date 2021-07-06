37996a9ec469508905832681eb198f5e8b42044a
sly03 <slydevwebproject@outlook.fr>
2021-07-06 17:00:27 +0200

    new title modifs

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Mon site depuis Netlify</title>
+        <title>Site NextJS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Mon premier site JS <code>pages/index.js</code>
         </p>
       </main>
 
